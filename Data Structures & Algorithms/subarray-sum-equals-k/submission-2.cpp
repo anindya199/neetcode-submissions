15cf62ddcc5d057055989c3c93df4e5ca2863772
anindya199 <anindya.sankarbag99@gmail.com>
2026-08-20 11:36:00 +0530

    Add: subarray-sum-equals-k - submission-2

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+
+        mp[0] = 1;
+        int cnt=0;
+        int sum=0;
+        int i;
+        for(i=0;i<nums.size();i++)
+        {
+            sum += nums[i];
+        
+            int val = sum - k;
+            if(mp.find(val) != mp.end())
+                cnt+=mp[val];
+            
+            mp[sum]++;
+        }
+        return cnt;
+    }
+};
