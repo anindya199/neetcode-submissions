b35271e23be3ba70561ce3c60648c865fc127ad4
anindya199 <anindya.sankarbag99@gmail.com>
2026-08-23 00:36:40 +0530

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int>len;
+    string encode(vector<string>& strs) {
+        string encoded;
+        for(string s: strs)
+        {
+            encoded += s;
+            len.push_back(s.size());
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string>decoded;
+        string temp="";
+        int i=0,j=len[0];
+        for(int k=0;k<len.size();k++)
+        {
+            j=i+len[k];
+            temp.clear();
+            while(i<j)
+            {
+                temp+=s[i];
+                i++;
+            }
+            decoded.push_back(temp);
+        }
+        // decoded.push_back(temp);
+        return decoded;
+    }
+};
